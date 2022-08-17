dba9496aaa7af7286ee7d1d0dd24d04ded53a250
dsa25 <73094994+dsa25@users.noreply.github.com>
2022-08-17 18:26:42 +0500

    likes

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,44 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="wr_likes">
+    <button v-on:click="addLIke">Like</button>
+    <button @click="Dizlike">Dizlike</button>
+    <div>conunt likes: {{ likes }}</div>
+    <div>conunt dizlikes: {{ dizlikes }}</div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dizlikes: 0,
+    }
+  },
+  methods: {
+    addLIke() {
+      this.likes += 1
+    },
+    Dizlike() {
+      this.dizlikes += 1
+    },
+  },
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+</script>
+
+<style>
+body {
+  margin: 0;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+* {
+  box-sizing: border-box;
+}
+.wr_likes {
+  padding: 30px;
+  margin: 0 auto;
+  width: 300px;
+}
+button {
+  margin: 0 3px;
 }
 </style>
