3b6de6253ffd0e04a461a3de158ecdd0714eeb52
dsa25 <73094994+dsa25@users.noreply.github.com>
2022-08-18 17:16:36 +0500

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="wr_likes">
-    <button v-on:click="addLIke">Like</button>
-    <button @click="Dizlike">Dizlike</button>
-    <div>conunt likes: {{ likes }}</div>
-    <div>conunt dizlikes: {{ dizlikes }}</div>
+  <div class="row">
+    <form @submit.prevent>
+      <span>{{ title }}</span>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="name"
+      />
+      <span>{{ text }}</span>
+      <input
+        v-bind:value="text"
+        @input="text = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="description"
+      />
+      <button @click="createPost">Create</button>
+    </form>
+    <div class="post" v-for="post in posts" :key="post.id">
+      <div><strong>name: </strong> {{ post.title }}</div>
+      <div><strong>descript: </strong> {{ post.text }}</div>
+    </div>
   </div>
 </template>
 
@@ -11,34 +30,64 @@
 export default {
   data() {
     return {
-      likes: 0,
-      dizlikes: 0,
+      posts: [
+        { id: 1, title: "js ", text: "text post 1" },
+        { id: 2, title: "js 2", text: "text post 2" },
+        { id: 3, title: "js 3", text: "text post 3" },
+      ],
+      title: "",
+      text: "",
     }
   },
   methods: {
-    addLIke() {
-      this.likes += 1
-    },
-    Dizlike() {
-      this.dizlikes += 1
+    createPost() {
+      this.posts.push({
+        id: this.posts.length,
+        title: this.title,
+        text: this.text,
+      })
+      this.title = ""
+      this.text = ""
     },
   },
 }
 </script>
 
 <style>
-body {
+text {
   margin: 0;
 }
 * {
   box-sizing: border-box;
 }
-.wr_likes {
-  padding: 30px;
+.row {
   margin: 0 auto;
-  width: 300px;
+  max-width: 800px;
+  padding: 0 15px;
+}
+.post {
+  padding: 15px;
+  border-radius: 5px;
+  border: 1px solid green;
+  margin: 15px 0;
+}
+form {
+  padding: 20px 50px;
+}
+.input {
+  display: block;
+  width: 100%;
+  padding: 7px 10px;
+  border-radius: 5px;
+  margin-bottom: 15px;
+  border: 1px solid green;
 }
 button {
-  margin: 0 3px;
+  margin: 0 0 0 auto;
+  display: block;
+  padding: 5px 8px;
+  border-radius: 3px;
+  border: 1px solid gray;
+  color: green;
 }
 </style>
