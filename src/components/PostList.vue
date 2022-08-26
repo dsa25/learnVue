5214976f677f52cd181dd1e99bb0df0ce1a4736d
dsa25 <73094994+dsa25@users.noreply.github.com>
2022-08-26 16:28:27 +0500

    animation list

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length">
     <h3>List post:</h3>
-    <PostItem
-      v-for="post in posts"
-      :key="post.id"
-      :post="post"
-      @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list">
+      <PostItem
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <div v-else style="text-align: center">Постов нет</div>
 </template>
@@ -20,3 +22,21 @@ export default {
   },
 }
 </script>
+<style>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(80px);
+}
+.post-list-move {
+  transition: transform 0.5s ease;
+}
+</style>
